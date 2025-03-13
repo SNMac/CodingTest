e136f53868229db2c6478f6051a3b04980683e59
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-03-13 19:47:54 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.86 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.swift
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.swift
@@ -3,18 +3,18 @@ import Foundation
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     var answer = [Int](repeating: 1_000_001, count: queries.count)
     
-    for i in 0..<queries.count {
-        let s = queries[i][0]
-        let e = queries[i][1]
-        let k = queries[i][2]
+    for (index, query) in queries.enumerated() {
+        let s = query[0]
+        let e = query[1]
+        let k = query[2]
         
         for j in s...e {
-            if arr[j] > k && arr[j] < answer[i] {
-                answer[i] = arr[j]
+            if arr[j] > k && arr[j] < answer[index] {
+                answer[index] = arr[j]
             }
         }
-        if answer[i] == 1_000_001 {
-            answer[i] = -1
+        if answer[index] == 1_000_001 {
+            answer[index] = -1
         }
     }
     
