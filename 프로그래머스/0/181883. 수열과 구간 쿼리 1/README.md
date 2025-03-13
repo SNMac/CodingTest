14510de0a8afc393f88020c5927b21a652199f62
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-03-13 19:45:15 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.69 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/README.md
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 1 - 181883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181883#) 
 
 ### 성능 요약
 
-메모리: 16.7 MB, 시간: 2.32 ms
+메모리: 16.7 MB, 시간: 1.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 13일 19:24:16
+2025년 03월 13일 19:45:14
 
 ### 문제 설명
 
--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.swift
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.swift
@@ -2,10 +2,11 @@ import Foundation
 
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     var answer = arr
-    for i in queries {
-        let start = i.first
-        let end = i.last
-        for j in start!...end! {
+    
+    for i in 0..<queries.count {
+        let s = queries[i][0]
+        let e = queries[i][1]
+        for j in s...e {
             answer[j] += 1
         }
     }
