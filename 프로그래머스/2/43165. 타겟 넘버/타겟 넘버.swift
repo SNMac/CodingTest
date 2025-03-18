6a887dbb7fb6e008951efec223dfd0068b022332
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-03-18 09:05:18 +0900

    [level 2] Title: 타겟 넘버, Time: 38.17 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -3,15 +3,15 @@ import Foundation
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     var answer = 0
     
-    func dfs(_ index: Int, _ curCalc: Int) {
+    func dfs(_ index: Int, _ currCalc: Int) {
         if index == numbers.count {
-            if curCalc == target {
+            if currCalc == target {
                 answer += 1
             }
             return
         }
-        dfs(index + 1, curCalc + numbers[index])
-        dfs(index + 1, curCalc - numbers[index])
+        dfs(index + 1, currCalc + numbers[index])
+        dfs(index + 1, currCalc - numbers[index])
     }
     
     dfs(0, 0)
