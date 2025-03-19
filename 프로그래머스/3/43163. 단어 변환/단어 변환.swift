196208df2305c3cc7ef4de8d7418717d29ef8d1c
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-03-19 12:10:38 +0900

    [level 3] Title: 단어 변환, Time: 2.23 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.swift
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -1,36 +1,32 @@
 import Foundation
 
 func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
-    var answerArr = [Int]()
     var isVisited = [Bool](repeating: false, count: words.count)
-    
-    if !words.contains(target) {
-        return 0
-    }
+    var queue = [(step: 0, currWord: begin)]
     
     func canTransferable(_ compare1: String, _ compare2: String) -> Bool {
         return zip(compare1, compare2).filter { $0 != $1 }.count == 1
     }
     
-    func dfs(currWord: String, step: Int) {
+    if !words.contains(target) {
+        return 0
+    }
+    
+    while !queue.isEmpty {
+        let curr = queue.removeFirst()
+        let currStep = curr.step
+        let currWord = curr.currWord
         if currWord == target {
-            answerArr.append(step)
-            return
+            return currStep
         }
         
         for (i, word) in words.enumerated() {
-            print("\(currWord), \(word), \(step)")
-            
             if !isVisited[i] && canTransferable(currWord, word) {
                 isVisited[i] = true
-                dfs(currWord: word, step: step + 1)
-                
-                isVisited[i] = false
+                queue.append((step: currStep + 1, currWord: word))
             }
         }
     }
     
-    dfs(currWord: begin, step: 0)
-    
-    return answerArr.min()!
+    return 0
 }
