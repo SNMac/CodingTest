90006a5c4aaa2dfbb03e84759687920e6209c335
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-03-25 23:41:38 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -2,35 +2,18 @@ import Foundation
 
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
     var answer = [Int]()
-    var etaArr = [Int]()
+    var lastReleaseDate = 0
     
     for i in 0..<progresses.count {
-        let remainProgress = 100 - progresses[i]
-        if remainProgress % speeds[i] > 0 {
-            etaArr.append(remainProgress / speeds[i] + 1)
+        let progress = Double(progresses[i])
+        let speed = Double(speeds[i])
+        let day = Int(ceil((100 - progress) / speed))
+        
+        if day > lastReleaseDate {
+            lastReleaseDate = day
+            answer.append(1)
         } else {
-            etaArr.append(remainProgress / speeds[i])
-        }
-    }
-    
-    while !etaArr.isEmpty {
-        var finished = 0
-        
-        for i in 0..<etaArr.count {
-            etaArr[i] -= 1
-        }
-        
-        while let first = etaArr.first {
-            if first <= 0 {
-                finished += 1
-                etaArr.removeFirst()
-            } else {
-                break
-            }
-        }
-        
-        if finished > 0 {
-            answer.append(finished)
+            answer[answer.count - 1] += 1
         }
     }
     
