0e9a6f8c63553615b98df9509804a2b702347836
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-03-26 12:50:31 +0900

    [level 3] Title: 가장 먼 노드, Time: 69.54 ms, Memory: 29.8 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.swift
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.swift
@@ -1,48 +1,40 @@
 import Foundation
 
 func solution(_ n:Int, _ edge:[[Int]]) -> Int {
-    // index번 노트의 최단경로 간선 개수를 저장하는 배열
-    var shortestRouteArr = [Int](repeating: 0, count: n + 1)
+    // 양방향 간선 배열
+    var connection = [[Int]](repeating: [Int](), count: n + 1)  // index: 출발 노드, value: 도착 노드
     var visited = [Bool](repeating: false, count: n + 1)
-    var vertexDict = [Int: [Int]]()
-    var queue: [(currNode: Int, count: Int)] = []
-    var answer = 0
+    var answerDic = [Int: Int]()  // key: 도착 노드, value: 1번 노드로부터 최단 거리
     
-    // 딕셔너리에 간선 양방향으로 추가
+    // 배열에 간선 양방향으로 추가
     for vertex in edge {
-        if vertexDict[vertex.first!] != nil {
-            vertexDict[vertex.first!]?.append(vertex.last!)
-        } else {
-            vertexDict[vertex.first!] = [vertex.last!]
-        }
-        
-        if vertexDict[vertex.last!] != nil {
-            vertexDict[vertex.last!]?.append(vertex.first!)
-        } else {
-            vertexDict[vertex.last!] = [vertex.first!]
-        }
+        connection[vertex.first!].append(vertex.last!)
+        connection[vertex.last!].append(vertex.first!)
     }
     
-    // BFS
-    queue.append((currNode: 1, count: 0))
-    visited[1] = true
-    while !queue.isEmpty {
-        let curr = queue.removeFirst()
-        let currCount = curr.count
-        let currNode = curr.currNode
-        shortestRouteArr[currNode] = currCount
-        
-        for nextNode in vertexDict[currNode]! {
-            if !visited[nextNode] {
-                visited[nextNode] = true
-                queue.append((currNode: nextNode, count: currCount + 1))
+    // BFS 시작
+    var currNodes = Set<Int>([1])  // 출발 노드를 담는 Set
+    var currDist = 1  // 1번 노드로부터의 거리
+    
+    while !currNodes.isEmpty {
+        var nodes = Set<Int>()  // 도착 노드를 담는 Set
+        for node in currNodes {
+            visited[node] = true
+            
+            for connectedNode in connection[node] {  // 모든 연결된 노드에 대해 반복
+                if !visited[connectedNode] {
+                    visited[connectedNode] = true
+                    answerDic[connectedNode] = currDist
+                    nodes.insert(connectedNode)
+                }
             }
         }
+        
+        currDist += 1
+        currNodes = nodes
     }
     
-    // 가장 먼 노드 개수 세기
-    let furthestRoute = shortestRouteArr.max()
-    answer = shortestRouteArr.filter { $0 == furthestRoute }.count
-    
-    return answer
+    let max = answerDic.values.max()
+    return answerDic.filter { $0.value == max }.count
 }
+
