3a962c00b05d0ebea91e4d027df333e6b917dc6e
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-04-02 10:40:32 +0900

    [level 2] Title: 피로도, Time: 27.52 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -4,30 +4,24 @@ func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
     var answer = 0
     var visited = [Bool](repeating: false, count: dungeons.count)
     
-    func dfs(currTired: Int, index: Int, count: Int) {
-        let consumeTired = dungeons[index].last!
-        let afterTired = currTired - consumeTired    
-        
+    func dfs(currTired: Int, count: Int) {  
         if answer < count {
             answer = count
         }
         
         for i in 0..<dungeons.count {
-            let targetMinNeedTired = dungeons[i].first!
-            if !visited[i] && afterTired >= targetMinNeedTired {
+            let targetMinNeedTired = dungeons[i][0]
+            let consumeTired = dungeons[i][1]
+            
+            if !visited[i] && currTired >= targetMinNeedTired {
                 visited[i] = true
-                dfs(currTired: afterTired, index: i, count: count + 1)
+                dfs(currTired: currTired - consumeTired, count: count + 1)
                 visited[i] = false
             }
         }
     }
     
-    for i in 0..<dungeons.count {
-        visited[i] = true
-        dfs(currTired: k, index: i, count: 1)
-        visited[i] = false
-    }
-    
+    dfs(currTired: k, count: 0)    
     
     return answer
 }
