0a29a8904ff2a0745224f58c5ace7215e94d9cb2
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-04-24 18:39:22 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1144.05 ms, Memory: 177 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.swift
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.swift
@@ -1,38 +1,38 @@
 import Foundation
 
 func solution(_ edges:[[Int]]) -> [Int] {
-    /// 간선 배열 [정점: [outgoing, incoming]]
-    var nodes = [Int:[Int]]()
-    /// [0] = 생성, [1] = 도넛, [2] = 막대, [3] = 8자
+    /// 간선 딕셔너리 [정점: [outgoing, incoming]]
+    var nodesDict = [Int:[Int]]()
+    /// [0] = 생성한 정점, [1] = 도넛 그래프 개수, [2] = 막대 그래프 개수, [3] = 8자 그래프 개수
     var answer = [0, 0, 0, 0]
     
     for edge in edges {
-        var outgoing = nodes[edge[0], default: [0, 0]]
+        var outgoing = nodesDict[edge[0], default: [0, 0]]
         outgoing[0] += 1
         
-        nodes[edge[0]] = outgoing
+        nodesDict[edge[0]] = outgoing
         
-        var incoming = nodes[edge[1], default: [0, 0]]
+        var incoming = nodesDict[edge[1], default: [0, 0]]
         incoming[1] += 1
-        nodes[edge[1]] = incoming
+        nodesDict[edge[1]] = incoming
     }
     
-    for node in nodes {
+    for node in nodesDict {
         let value = node.value
-        let outgoint = value[0]
+        let outgoing = value[0]
         let incoming = value[1]
         
-        if outgoint > 1 && incoming == 0 {  // 생성
+        if outgoing > 1 && incoming == 0 {  // 생성한 정점
             answer[0] = node.key
-        } else if outgoint > 1 && incoming > 1 {  // 8자
+        } else if outgoing > 1 && incoming > 1 {  // 8자 그래프 개수
             answer[3] += 1
-        } else if outgoint == 0 {  // 막대
+        } else if outgoing == 0 {  // 막대 그래프 개수
             answer[2] += 1
         }
     }
     
-    let graph = nodes[answer[0]]![0]  // 생성한 정점의 outgoing 개수
-    answer[1] = graph - answer[2] - answer[3]
+    let totalGraph = nodesDict[answer[0]]![0]  // 총 그래프 개수 = 생성한 정점의 outgoing 개수
+    answer[1] = totalGraph - answer[2] - answer[3]  // 도넛 그래프 개수
     
     return answer
 }
