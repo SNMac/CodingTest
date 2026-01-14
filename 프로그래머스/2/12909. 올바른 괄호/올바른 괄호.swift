5ed1ce4d6ab8ad21832126adca49cb0f18df684a
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-01-14 14:18:42 +0900

    [level 2] Title: 올바른 괄호, Time: 11.23 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ s:String) -> Bool {
+    var stack: [Character] = []
+    
+    for c in s {
+        if (c == "(") {
+            stack.append(c)
+        } else {
+            if stack.isEmpty { return false }
+            stack.removeLast()
+        }
+    }
+    
+    return stack.isEmpty
+}
