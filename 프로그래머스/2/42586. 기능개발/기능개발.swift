1d3d39b7097c4497e800b96ffd72aac5466c1a1e
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-02-21 17:37:01 +0900

    [level 2] Title: 기능개발, Time: 0.26 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,21 +1,27 @@
 import Foundation
 
-func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    var answer = [Int]()
-    var lastReleaseDate = 0
+func solution(_ progresses: [Int], _ speeds: [Int]) -> [Int] {
     
-    for i in 0..<progresses.count {
-        let progress = Double(progresses[i])
-        let speed = Double(speeds[i])
-        let day = Int(ceil((100 - progress) / speed))
-        
-        if day > lastReleaseDate {
-            lastReleaseDate = day
-            answer.append(1)
-        } else {
-            answer[answer.count - 1] += 1
-        }
+    var inProgressArr: [Int] = progresses
+    var etaArr: [Int] = []
+    for (index, progress) in inProgressArr.enumerated() {
+        let eta: Double = Double(100 - progress) / (Double)(speeds[index])
+        etaArr.append(Int(eta.rounded(.up)))
     }
     
-    return answer
+    var deployment: [Int] = []
+    while !etaArr.isEmpty {
+        let first = etaArr.first!
+        etaArr.remove(at: 0)
+        etaArr = etaArr.map { $0 - first }
+        
+        var deploy = 1
+        while !etaArr.isEmpty && etaArr.first! <= 0 {
+            deploy += 1
+            etaArr.removeFirst()
+        }
+        deployment.append(deploy)
+    }
+    
+    return deployment
 }
