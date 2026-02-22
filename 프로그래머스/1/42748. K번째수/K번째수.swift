af1ce54af96bad1b60100aeffc70d48596efac15
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-02-22 16:24:39 +0900

    [level 1] Title: K번째수, Time: 0.15 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -2,15 +2,14 @@ import Foundation
 
 func solution(_ array: [Int], _ commands: [[Int]]) -> [Int] {
     var result: [Int] = []
-    
     for command in commands {
-        let i = command[0]
-        let j = command[1]
-        let k = command[2]
+        let i = command[0] - 1
+        let j = command[1] - 1
+        let k = command[2] - 1
         
-        let sliced = array[(i - 1)...(j - 1)]
-        let sorted = sliced.sorted()
-        result.append(sorted[k - 1])
+        let sliced = array[i...j]
+        let sorted = Array(sliced).sorted()
+        result.append(sorted[k])
     }
     
     return result
