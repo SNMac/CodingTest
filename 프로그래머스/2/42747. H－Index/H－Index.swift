af1220d0081c81226cd363ccd9f4021300865753
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-02-22 19:55:22 +0900

    [level 2] Title: H-Index, Time: 2.60 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.swift
+++ 프로그래머스/2/42747. H－Index/H－Index.swift
@@ -7,12 +7,16 @@ func solution(_ citations: [Int]) -> Int {
     for (index, citation) in sorted.enumerated() {
         let paperIndex = index + 1
         
+        // paperIndex(index + 1)가 citation보다 커지기 시작할 때
+        // => 그 직전 paperIndex(index)가 H-Index
         if paperIndex > citation {
             result = index
             break
         }
     }
     
+    // result가 변경되지 않은 경우
+    // => 발표된 논문 모두 citation이 paperIndex보다 크므로 논문 개수가 H-Index
     if result == -1 {
         result = sorted.count
     }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 16.3 MB, 시간: 3.57 ms
+메모리: 16.6 MB, 시간: 2.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 22일 19:51:40
+2026년 02월 22일 19:55:20
 
 ### 문제 설명
 
