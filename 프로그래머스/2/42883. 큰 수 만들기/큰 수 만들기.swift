79fb0784f85d5caabc4cad170c6b2260dd6cc4a5
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-02-24 03:53:09 +0900

    [level 2] Title: 큰 수 만들기, Time: 184.28 ms, Memory: 20.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
@@ -1,27 +1,17 @@
 import Foundation
 
-func solution(_ number:String, _ k:Int) -> String {
-    var stack = [Character]()
-    let totalStackLength = number.count - k
-    var remainCount = k
+func solution(_ number: String, _ k: Int) -> String {
+    var stack: [Character] = []
+    var deleteRemain = k
     
     for num in number {
-        // 스택의 top이 num보다 더 작으면
-        // ➡️ 스택이 비어있지 않고
-        // ➡️ 스택의 top이 num보다 작고
-        // ➡️ 제거할 수 있는 개수가 남아있는 동안
-        // 스택의 top을 지워나감(remainCount -= 1)
-        while !stack.isEmpty && stack.last! < num && remainCount > 0 {
+        while deleteRemain != 0 && !stack.isEmpty && num > stack.last! {
             stack.removeLast()
-            remainCount -= 1
+            deleteRemain -= 1
         }
         
-        // 스택에 아직 더 담을 수 있는 경우(스택의 길이 < number의 길이에서 k를 뺀 값)
-        // 스택에 num을 담음
-        if stack.count < totalStackLength {
-            stack.append(num)
-        }
+        stack.append(num)
     }
     
-    return String(stack)
+    return String(stack.prefix(stack.count - deleteRemain))
 }
