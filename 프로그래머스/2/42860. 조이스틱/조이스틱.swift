bb96e27c1461832a84a61169d4777930508ece1d
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-02-23 23:55:34 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.swift
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.swift
@@ -1,35 +1,32 @@
 import Foundation
 
-func solution(_ name:String) -> Int {
-    let aAscii: UInt8 = 65
-    let zAscii: UInt8 = 90
-    let name = name.map { $0 }
+func solution(_ name: String) -> Int {
+    var nameChar: [Character] = Array(name)
     
-    // 수직 이동에 대한 최솟값
-    var verticalMove = 0
-    // 수평 이동에 대한 최솟값
-    // - 한 방향으로만 갈 때 = 문자열 길이 - 1
-    var horizontalMove = name.count - 1
-    for i in 0..<name.count {
-        // 수직 이동에 대한 최솟값 계산
-        let charAscii = name[i].asciiValue!
-        let dist = min(charAscii - aAscii, zAscii + 1 - charAscii)
-        verticalMove += Int(dist)
-        
-        // 연속된 A의 끝을 계산
-        var aEndIndex = i + 1
-        while aEndIndex < name.count && name[aEndIndex] == "A" {
-            aEndIndex += 1
-        }
-        
-        // 문자열 끝부터 연속된 A 중 마지막 인덱스까지의 거리
-        let aEndDistFromLast = name.count - aEndIndex
-        // 시작점[0]에서 연속된 A 끝 뒤로 이동(왼쪽) ➡️ 다시 시작점[0]으로 돌아감(오른쪽) ➡️ 시작점[0]에서 연속된 A 시작 전까지 이동(오른쪽)
-        let backAndForward = aEndDistFromLast * 2 + i
-        // 시작점[0]에서 연속된 A 시작 전까지 이동(오른쪽) ➡️ 다시 시작점[0]으로 돌아감(왼쪽) ➡️ 시작점[0]에서 연속된 A 끝 뒤로 이동(왼쪽)
-        let forwardAndBack = i * 2 + aEndDistFromLast
-        horizontalMove = min(horizontalMove, backAndForward, forwardAndBack)
+    var upDown = 0
+    var leftRight = nameChar.count - 1
+    
+    func calcMove(_ char: Character) -> Int {
+        // 위로 이동: char - 65
+        // 아래로 이동: 91 - char
+        let upMove = Int(char.asciiValue! - 65)
+        let downMove = Int(91 - char.asciiValue!)
+        return min(upMove, downMove)
     }
     
-    return verticalMove + horizontalMove
+    for index in nameChar.indices {
+        upDown += calcMove(nameChar[index])
+        
+        var endOfAIndex = index + 1
+        while endOfAIndex < nameChar.count && nameChar[endOfAIndex] == "A" {
+            endOfAIndex += 1
+        }
+        
+        let leftMove = index * 2 + (nameChar.count - endOfAIndex)
+        let rightMove = (nameChar.count - endOfAIndex) * 2 + index
+        
+        leftRight = min(leftRight, leftMove, rightMove)
+    }
+    
+    return upDown + leftRight
 }
