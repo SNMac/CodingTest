f98f894cc35b006419c1b2cb83ff5d6c72c3a128
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:44:16 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+bool isUsed[9];
+
+void dfs(int cur_digit) {
+    if (cur_digit == m + 1) {
+        for (int i = 1; i <= m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        isUsed[arr[cur_digit - 1]] = true;
+        return;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!isUsed[i] && arr[cur_digit - 1] <= i) {
+            arr[cur_digit] = i;
+            dfs(cur_digit + 1);
+            isUsed[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    dfs(1);
+}
