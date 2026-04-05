747bd35afff462ed4675a917531d0e71b5f9c7bb
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:51:21 +0900

    [Silver IV] Title: 접미사 배열, Time: 4 ms, Memory: 2688 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.cc
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.cc
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
+#include <set>
 
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
 using namespace std;
 
 string input;
-vector<string> v;
+set<string> s;
 
 int main() {
     FAST_IO
@@ -16,9 +15,8 @@ int main() {
     getline(cin, input);
 
     for (int i = 0; i < input.length(); i++)
-        v.push_back(input.substr(i, input.length() - i));
-    sort(v.begin(), v.end());
+        s.insert(input.substr(i, input.length() - i));
 
-    for (string& str : v)
-        cout << str << '\n';
+    for (auto& it : s)
+        cout << it << '\n';
 }
