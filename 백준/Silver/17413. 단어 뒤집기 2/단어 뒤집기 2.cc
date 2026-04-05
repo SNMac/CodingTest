1e37e40e63b75fb33b7a34f354142dbd9b215974
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:24:25 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 4 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+using namespace std;
+
+int main() {
+    FAST_IO
+
+    string S;
+    getline(cin, S);
+
+    for (int i = 0; i < S.length(); i++) {
+        if (S[i] == '<') {
+            i = S.find('>', i);
+        } else {  // S[i] == character
+            int tag_idx = S.find('<', i);
+            if (tag_idx != string::npos) {
+                int space_idx = S.find(' ', i);
+                if (space_idx != string::npos) {
+                    if (space_idx < tag_idx) {
+                        reverse(S.begin() + i, S.begin() + space_idx);
+                        i = space_idx;
+                    } else {  // tag_idx <= space_idx
+                        reverse(S.begin() + i, S.begin() + tag_idx);
+                        i = S.find('>', i);
+                    }
+                } else {  // space_idx == string::npos
+                    reverse(S.begin() + i, S.begin() + tag_idx);
+                    i = S.find('>', i);;
+                }
+            } else {  // tag_idx == string::npos
+                int space_idx = S.find(' ', i);
+                if (space_idx != string::npos) {
+                    reverse(S.begin() + i, S.begin() + space_idx);
+                    i = space_idx;
+                } else {  // space_idx == string::npos
+                    reverse(S.begin() + i, S.end());
+                    break;
+                }
+            }
+        }
+    }
+    cout << S << '\n';
+}
