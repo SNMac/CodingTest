bc518fc45db5c8170939ab5702d4289166c2112e
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:33:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -7,30 +7,13 @@ int main() {
     cin >> n;
     int count = n / 5;
     int rest = n % 5;
-    if (count == 0) {
-        switch (rest) {
-            case 3 :
-                count = 1;
-                break;
-            case 4 :
-                count = -1;
-                break;
-        }
-    } else if (count == 1) {
-        switch (rest) {
-            case 1 :
-                count++;
-                break;
-            case 2 :
-                count = -1;
-                break;
-            case 3 :
-                count++;
-                break;
-            case 4 :
-                count += 2;
-                break;
-        }
+    if (count < 2) {
+        if (n == 4 || n == 7)
+            count = -1;
+        else if (n == 3 || n == 6 || n == 9)
+            count = n / 3;
+        else if (n == 8)
+            count++;
     } else {
         switch (rest) {
             case 1 :
