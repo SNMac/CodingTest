efd5f943dc2052a865e8e75dd2eecd4bce4853ca
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:41:39 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -1,33 +1,29 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    bool arr[1001] = {false, };
+    queue<int> q;
     int n, k;
     cin >> n >> k;
     for (int i = 1; i <= n; i++)
-        arr[i] = true;
+        q.push(i);
 
     cout << '<';
-    int order = 1, i = 1, alive = n;
-    while(alive) {
-        if (arr[i]) {
-            if (order % k == 0) {
-                cout << i;
-                order = 1;
-                arr[i] = false;
-                if (--alive == 0)
-                    cout << '>';
-                else
-                    cout << ", ";
-            } else {
-                order++;
-            }
+    while(true) {
+        for (int i = 1; i < k; i++) {
+            q.push(q.front());
+            q.pop();
         }
-        if (++i > n)
-            i = 1;
+        cout << q.front();
+        q.pop();
+        if (q.empty()) {
+            cout << '>';
+            return 0;
+        } else
+            cout << ", ";
     }
 }
