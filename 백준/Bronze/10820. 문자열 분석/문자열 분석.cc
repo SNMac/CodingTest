f758acef4cc506d94364d84d56837b9e884eb7b8
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:23:57 +0900

    [Bronze II] Title: 문자열 분석, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+using namespace std;
+
+string input;
+int lower, upper, digit, space;
+
+int main() {
+    FAST_IO
+
+    while (getline(cin, input)) {
+        for (int i = 0; i < input.length(); i++) {
+            if (islower(input[i])) {
+                lower++;
+            } else if (isupper(input[i])) {
+                upper++;
+            } else if (isdigit(input[i])) {
+                digit++;
+            } else if (input[i] == ' ') {
+                space++;
+            }
+        }
+
+        cout << lower << ' ' << upper << ' ' << digit << ' ' << space << '\n';
+        lower = upper = digit = space = 0;
+    }
+}
