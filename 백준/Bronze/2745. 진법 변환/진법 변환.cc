7f2fc5096d3cb2124c53d41d4e09ec059dee0e0d
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:45:45 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string n;
+    int b;
+    cin >> n >> b;
+    for (auto& it : n) {
+        it = tolower(it);
+    }
+
+    int total = 0;
+    for (int i = n.length() - 1; i >= 0; i--) {
+        int temp = 1;
+        for (int j = 0; j < i; j++) {
+            temp *= b;
+        }
+        int nowIndex = n.length() - 1 - i;
+        if (isalpha(n[nowIndex])) {
+            temp *= n[nowIndex] - 'a' + 10;
+        } else {
+            temp *= n[nowIndex] - '0';
+        }
+        total += temp;
+    }
+    cout << total << '\n';
+}
