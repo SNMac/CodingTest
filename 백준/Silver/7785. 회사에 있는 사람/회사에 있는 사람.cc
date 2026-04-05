5cf5401592725f1ea2293bb47e0474794af7a987
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:26:58 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 116 ms, Memory: 9816 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n;
+    set<string, greater<string>> s;
+    cin >> n;
+    cin.ignore();
+    for (int i = 0; i < n; i++) {
+        string input, name, log;
+        getline(cin, input);
+        name = input.substr(0, input.find(' '));
+        log = input.substr(input.find(' ') + 1);
+        if (log == "enter") {
+            s.insert(name);
+        } else {
+            s.erase(name);
+        }
+    }
+    for (auto& it : s)
+        cout << it << '\n';
+}
