6269c840800c8c0fc5d87d4ef88cbdfdffbe9810
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:36:13 +0900

    [Silver V] Title: 숫자 카드, Time: 216 ms, Memory: 5892 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -7,22 +7,17 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<int> ov;
-    vector<int> cv;
     int n, m;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        int o_input;
-        cin >> o_input;
-        ov.push_back(o_input);
-    }
+    vector<int> ov(n);
+    for (int i = 0; i < n; i++)
+        cin >> ov[i];
     sort(ov.begin(), ov.end());
+
     cin >> m;
-    for (int i = 0; i < m; i++) {
-        int c_input;
-        cin >> c_input;
-        cv.push_back(c_input);
-    }
+    vector<int> cv(m);
+    for (int i = 0; i < m; i++)
+        cin >> cv[i];
 
     for (int i = 0; i < m; i++) {
         auto cmp = binary_search(ov.begin(), ov.end(), cv[i]);
