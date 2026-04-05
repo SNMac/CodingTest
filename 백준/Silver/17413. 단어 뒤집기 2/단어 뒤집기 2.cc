4882d9a5e82021498363f351f277bdd1a3d48a57
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:50:27 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 8 ms, Memory: 2536 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
@@ -9,39 +8,45 @@ using namespace std;
 int main() {
     FAST_IO
 
-    string S;
+    string S, result;
     getline(cin, S);
-
-    for (int i = 0; i < S.length(); i++) {
-        if (S[i] == '<') {
-            i = S.find('>', i);
-        } else {  // S[i] == character
-            int tag_idx = S.find('<', i);
-            if (tag_idx != string::npos) {
-                int space_idx = S.find(' ', i);
-                if (space_idx != string::npos) {
-                    if (space_idx < tag_idx) {
-                        reverse(S.begin() + i, S.begin() + space_idx);
-                        i = space_idx;
-                    } else {  // tag_idx <= space_idx
-                        reverse(S.begin() + i, S.begin() + tag_idx);
-                        i = S.find('>', i);
-                    }
-                } else {  // space_idx == string::npos
-                    reverse(S.begin() + i, S.begin() + tag_idx);
-                    i = S.find('>', i);;
+    while (!S.empty()) {
+        if (S.front() == '<') {
+            result += (S.substr(0, S.find('>') + 1));
+            S = S.substr(S.find('>') + 1);
+        } else {
+            int space_idx = S.find(' ');
+            int tag_idx = S.find('<');
+            if (space_idx == string::npos) {
+                if (tag_idx == string::npos) {
+                    for (int i = S.length() - 1; i >= 0; i--)
+                        result += S[i];
+                    S.erase(0);
+                } else {  // tag_idx != string::npos
+                    for (int i = tag_idx - 1; i >= 0; i--)
+                        result += S[i];
+                    S.erase(0, tag_idx);
                 }
-            } else {  // tag_idx == string::npos
-                int space_idx = S.find(' ', i);
-                if (space_idx != string::npos) {
-                    reverse(S.begin() + i, S.begin() + space_idx);
-                    i = space_idx;
-                } else {  // space_idx == string::npos
-                    reverse(S.begin() + i, S.end());
-                    break;
+            } else {  // space_idx != string::npos
+                if (tag_idx == string::npos) {
+                    for (int i = space_idx - 1; i >= 0; i--)
+                        result += S[i];
+                    result += ' ';
+                    S.erase(0, space_idx + 1);
+                } else {  // tag_idx != string::npos
+                    if (space_idx < tag_idx) {
+                        for (int i = space_idx - 1; i >= 0; i--)
+                            result += S[i];
+                        result += ' ';
+                        S.erase(0, space_idx + 1);
+                    } else {  // tag_idx <= space_idx
+                        for (int i = tag_idx - 1; i >= 0; i--)
+                            result += S[i];
+                        S.erase(0, tag_idx);
+                    }
                 }
             }
         }
     }
-    cout << S << '\n';
+    cout << result << '\n';
 }
