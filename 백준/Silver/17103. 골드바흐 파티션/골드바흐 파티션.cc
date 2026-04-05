01822874a604495a6aad67f57953bbead237c57b
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:48:00 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 196 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
@@ -3,25 +3,24 @@
 #define MAX 1000001
 using namespace std;
 
-bool notPrimeArr[MAX];
+bool prime[MAX];
 
 void primeInit() {
-    notPrimeArr[0] = notPrimeArr[1] = true;
+    fill_n(prime, MAX, true);
+    prime[0] = prime[1] = false;
     for (int i = 2; i < MAX; i++) {
-        if (notPrimeArr[i])
+        if (!prime[i])
             continue;
 
-        bool isPrime = true;
         for (int j = 2; j * j <= i; j++) {  // 소수 판별
             if (i % j == 0) {
-                isPrime = false;
+                prime[i] = false;
                 break;
             }
         }
-        if (isPrime) {
-            for (int j = i + i; j < MAX; j += i) {
-                notPrimeArr[j] = true;
-            }
+        if (prime[i]) {
+            for (int j = i + i; j < MAX; j += i)
+                prime[j] = false;
         }
     }
 }
@@ -29,7 +28,7 @@ void primeInit() {
 int countGold(int n) {
     int total = 0;
     for (int i = 2; i <= n / 2; i++) {
-        if (!notPrimeArr[i] & !notPrimeArr[n - i])
+        if (prime[i] & prime[n - i])
             total++;
     }
     return total;
