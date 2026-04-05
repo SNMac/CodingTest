6f1a5ea087de089b17f5900eaa4adc7570a0a22d
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:50:24 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -8,19 +8,26 @@ using namespace std;
 int main() {
     FAST_IO
 
-    queue<int> q;
+    queue<int> q1;
+    queue<int> q2;
     int N, K;
     cin >> N >> K;
     for (int i = 1; i <= N; i++)
-        q.push(i);
-    cout << '<';
-    while (q.size() != 1) {
+        q1.push(i);
+    while (!q1.empty()) {
         for (int j = 1; j < K; j++) {
-            q.push(q.front());
-            q.pop();
+            q1.push(q1.front());
+            q1.pop();
         }
-        cout << q.front() << ", ";
-        q.pop();
+        q2.push(q1.front());
+        q1.pop();
     }
-    cout << q.front() << ">\n";
+    cout << '<' << q2.front();
+    while (true) {
+        q2.pop();
+        if (q2.empty())
+            break;
+        cout << ", " << q2.front();
+    }
+    cout << ">\n";
 }
