3dc49eb07b3212b5e2800915d2c054205355653b
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:34:18 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int n) {
+    int result = n;
+    if (n == 0 || n == 1)
+        return n;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fib(n) << '\n';
+}
