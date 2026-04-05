a75cf12e6d980ee968c67d030f294f26a533d730
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:44:22 +0900

    [Gold IV] Title: N-Queen, Time: 1596 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,7 +3,9 @@
 using namespace std;
 
 int n, ans;
-int col[15];  // col[row] = y, (row, y)
+int col_arr[15];  // col_arr[col] = row, (col, row)
+int up_diagonal[30];
+int down_diagonal[30];
 
 void dfs(int row) {
     if (row == n + 1) {
@@ -11,21 +13,17 @@ void dfs(int row) {
         return;
     }
 
-    for (int cur_col = 1; cur_col <= n; cur_col++) {
-        if (col[row] == 0) {  // coordinate is available
-            bool valid = true;
-            for (int prev_row = 1; prev_row < row; prev_row++) {  // check
-                if (col[prev_row] == cur_col || abs(prev_row - row) == abs(col[prev_row] - cur_col)) {
-                    valid = false;
-                    break;
-                }
-            }
-            if (valid) {
-                col[row] = cur_col;
-                dfs(row + 1);
-                col[row] = 0;
-            }
-        }
+    for (int col = 1; col <= n; col++) {
+        if (col_arr[col] || up_diagonal[col + row] || down_diagonal[col - row + n - 1])
+            continue;
+        
+        col_arr[col] = row;
+        up_diagonal[col + row] = 1;
+        down_diagonal[col - row + n - 1] = 1;
+        dfs(row + 1);
+        col_arr[col] = 0;
+        up_diagonal[col + row] = 0;
+        down_diagonal[col - row + n - 1] = 0;
     }
 }
 
