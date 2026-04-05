4ab64f302592796365bbcb12f29ae9311624691c
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:41:46 +0900

    [Silver IV] Title: 카드2, Time: 44 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    list<int> l;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        l.push_back(i);
+
+    while(l.size() != 1) {
+        l.pop_front();
+        l.push_back(l.front());
+        l.pop_front();
+    }
+    cout << l.front() << '\n';
+}
