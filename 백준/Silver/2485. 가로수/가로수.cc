566e4051502d52ce226daafb5d6c797cc45d1091
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:47:09 +0900

    [Silver IV] Title: 가로수, Time: 60 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.cc
+++ 백준/Silver/2485. 가로수/가로수.cc
@@ -14,27 +14,24 @@ int gcd(int a, int b) {
 int main() {
     int n;
     cin >> n;
+    vector<int> loc(n);
     vector<int> diff;
-    int temp = 0;
     for (int i = 0; i < n; i++) {
         int input;
         cin >> input;
-        if (temp != 0) {
-            temp = input - temp;
-            diff.push_back(temp);
-        } else {
-
-        }
-        temp = input;
+        loc[i] = input;
+    }
+    for (int i = 0; i < n - 1; i++) {
+        diff.push_back(loc[i + 1] - loc[i]);
     }
 
-    int gcd_result = diff.at(0);
-    for (int i = 1; i < diff.size(); i++) {
-        gcd_result = gcd(gcd_result, diff.at(i));
+    int gcd_result = gcd(diff[0], diff[1]);
+    for (int i = 2; i < diff.size(); i++) {
+        gcd_result = gcd(gcd_result, diff[i]);
     }
     int result = 0;
-    for (int i : diff) {
-        result += i / gcd_result - 1;
+    for (int i = 0; i < diff.size(); i++) {
+        result += diff[i] / gcd_result - 1;
     }
     cout << result << '\n';
 }
