a48c493c38e61c01a04fe7549545f71a3f9e1b00
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:35:09 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -9,7 +8,17 @@ int main() {
     int* arr = new int[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    sort(arr, arr + n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
     for (int i = 0; i < n; i++)
         cout << arr[i] << '\n';
     delete[] arr;
