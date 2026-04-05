1ed545828db03223ea3444cde003c5513a3de99a
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:31:41 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+   int* arr = new int[10];
+   for (int i = 0; i < 10; i++)
+       cin >> arr[i];
+
+    for (int i = 0; i < 10; i++)
+        arr[i] %= 42;
+
+    bool diff;
+    int res = 0;
+    for (int i = 0; i < 10; i++) {
+        diff = true;
+        for (int j = i + 1; j < 10; j++) {
+            if (arr[j] == arr[i])
+                diff = false;
+        }
+        if (diff)
+            res++;
+    }
+
+    cout << res << endl;
+    delete[] arr;
+}
