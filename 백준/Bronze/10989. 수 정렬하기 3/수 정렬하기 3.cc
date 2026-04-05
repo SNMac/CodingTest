75b275a657070d19a2c3b927edb578740cbfab6e
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:35:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1776 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,16 +1,20 @@
-#include <cstdio>
+#include <iostream>
+
 #define MAX 10001
 
 using namespace std;
 
 int main() {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
     int n;
-    scanf("%d", &n);
-    int count[MAX] = {0, };
+    cin >> n;
+    int count[MAX];
+    fill_n(count, MAX, 0);
     int max = 1;
     int input;
     for (int i = 0; i < n; i++) {
-        scanf("%d", &input);
+        cin >> input;
         count[input]++;
         if (input > max)
             max = input;
@@ -18,7 +22,7 @@ int main() {
 
     for (int i = 1; i <= max; i++) {
         while(count[i]) {
-            printf("%d\n", i);
+            cout << i << '\n';
             count[i]--;
         }
     }
