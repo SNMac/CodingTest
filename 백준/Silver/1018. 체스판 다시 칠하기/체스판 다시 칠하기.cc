a12ed1e927362b39a9a38b88f848cda2299aa75c
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:34:52 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    string* board = new string[n];
+    for (int i = 0; i < n; i++)
+        cin >> board[i];
+
+    bool cont = true;
+    int startY = 0, startX = 0;
+    int min = -1;
+    while (cont) {
+        int countB = 0;
+        int countW = 0;
+        for (int y = startY; y < startY + 8; y++) {
+            for (int x = startX; x < startX + 8; x++) {
+                if ((y - startY) % 2 == 0) {
+                    if ((x - startX) % 2 == 0) {
+                        if (board[y][x] == 'B')
+                            countW++;
+                        else
+                            countB++;
+                    } else {
+                        if (board[y][x] == 'B')
+                            countB++;
+                        else
+                            countW++;
+                    }
+                } else {
+                    if ((x - startX) % 2 == 0) {
+                        if (board[y][x] == 'B')
+                            countB++;
+                        else
+                            countW++;
+                    } else {
+                        if (board[y][x] == 'B')
+                            countW++;
+                        else
+                            countB++;
+                    }
+                }
+            }
+        }
+        if (startX + 1 + 8 <= m) {
+            startX++;
+        } else {
+            startX = 0;
+            if (startY + 1 + 8 <= n)
+                startY++;
+            else
+                cont = false;
+        }
+
+        if (min == -1) {
+            if (countB < countW)
+                min = countB;
+            else
+                min = countW;
+        } else {
+            if (countB < countW) {
+                if (min > countB)
+                    min = countB;
+            } else {
+                if (min > countW)
+                    min = countW;
+            }
+        }
+    }
+    cout << min << '\n';
+    delete[] board;
+}
