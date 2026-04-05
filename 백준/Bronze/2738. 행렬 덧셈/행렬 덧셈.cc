6deea670b7c31dfb15e09fdbedf7b265909a439f
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:43:21 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -5,33 +5,27 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    int** mat1 = new int*[n];
-    int** mat2 = new int*[n];
-    for (int i = 0; i < n; i++) {
-        mat1[i] = new int[m];
-        mat2[i] = new int[m];
-    }
+    int** mat = new int*[n];
+    for (int i = 0; i < n; i++)
+        mat[i] = new int[m];
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> mat1[i][j];
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> mat2[i][j];
+            cin >> mat[i][j];
         }
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            mat1[i][j] += mat2[i][j];
+            int input;
+            cin >> input;
+            mat[i][j] += input;
         }
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cout << mat1[i][j] << ' ';
+            cout << mat[i][j] << ' ';
         }
         cout << '\n';
     }
