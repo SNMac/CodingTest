a60ba6d8c33fb12211970dda71a69a9547c35671
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:43:52 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        unordered_map<string, int> m;
+        int n;
+        cin >> n;
+        cin.ignore();
+        for (int j = 0; j < n; j++) {
+            string input, cloth, kinds;
+            getline(cin, input);
+            kinds = input.substr(input.find(' ') + 1);
+            if (m.find(kinds) == m.end()) {
+                m.insert({kinds, 1});
+            } else {
+                m[kinds]++;
+            }
+        }
+
+        int ans = 1;
+        for (const auto& it : m)
+            ans *= it.second + 1;
+        ans--;
+        cout << ans << '\n';
+    }
+}
