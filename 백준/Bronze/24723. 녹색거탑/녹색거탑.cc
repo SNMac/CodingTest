e9aef4d15ad031b110c971f0331d08d9aabb4351
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:25:48 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    int result = 1;
+    for (int i = 0; i < n; i++)
+        result <<= 1;
+    cout << result << '\n';
+}
