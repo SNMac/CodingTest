a30c4bd5d0b864108494182fd13311abac16360f
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:49:33 +0900

    [Silver I] Title: 스타트와 링크, Time: 80 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -10,34 +10,34 @@ bool selected[21];
 int S[21][21];
 int N;
 int s_member[10];
-vector<int> s_stat;
-vector<int> l_stat;
 set<int> sl_diff;
 
 void dfs(int count, int index) {
     if (count == N / 2) {
-        int s_temp = 0;
+        int s_stat = 0;
         for (int i = 0; i < N / 2 - 1; i++) {  // 다 뽑은 상태
             for (int j = i + 1; j < N / 2; j++) {
                 int s1 = s_member[i], s2 = s_member[j];
-                s_temp += S[s1][s2] + S[s2][s1];
+                s_stat += S[s1][s2] + S[s2][s1];
             }
         }
-        s_stat.push_back(s_temp);
 
         vector<int> l_member;
         for (int i = 1; i <= N; i++) {
             if (!selected[i])
                 l_member.push_back(i);
         }
-        int l_temp = 0;
+        int l_stat = 0;
         for (int i = 0; i < l_member.size() - 1; i++) {
             for (int j = i + 1; j < l_member.size(); j++) {
                 int l1 = l_member[i], l2 = l_member[j];
-                l_temp += S[l1][l2] + S[l2][l1];
+                l_stat += S[l1][l2] + S[l2][l1];
             }
         }
-        l_stat.push_back(l_temp);
+        int diff = s_stat - l_stat;
+        if (diff < 0)
+            diff *= -1;
+        sl_diff.insert(diff);
         return;
     }
 
@@ -60,11 +60,5 @@ int main() {
         }
     }
     dfs(0, 1);
-    for (int i = 0; i < s_stat.size(); i++) {
-        int diff = s_stat[i] - l_stat[i];
-        if (diff < 0)
-            diff *= -1;
-        sl_diff.insert(diff);
-    }
     cout << *sl_diff.begin() << '\n';
 }
