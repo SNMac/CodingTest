e88a064b164427066baba89f7288e6cc6316d0b7
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:45:26 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
@@ -8,25 +8,18 @@ int main() {
     int n, m;
     cin >> n >> m;
     vector<int> v;
-    v.push_back(0);
 
     for (int a = 1; a <= n; a++)
         v.push_back(a);
 
     for (int a = 0; a < m; a++) {
-        vector<int> tempV;
         int i, j, k;
         cin >> i >> j >> k;
-        for (int b = i; b <= j; b++)
-            tempV.push_back(v[b]);
-        rotate(tempV.begin(), tempV.begin() + (k - i), tempV.end());
-        int tempIndex = 0;
-        for (int b = i; b <= j; b++)
-            v[b] = tempV[tempIndex++];
+        rotate(v.begin() + (i - 1), v.begin() + (k - 1), v.begin() + j);
     }
 
-    for (int i = 1; i <= n; i++) {
-        cout << v[i] << ' ';
+    for (int it : v) {
+        cout << it << ' ';
     }
     cout << '\n';
 }
