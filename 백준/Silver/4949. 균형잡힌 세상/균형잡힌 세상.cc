2228148aded5d41b63b61e41f5df0f1e3281800f
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:42:10 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string str;
+    while(true) {
+        stack<char> s;
+        getline(cin, str);
+        bool exit = false;
+        if (str == ".")
+            break;
+        for (char c : str) {
+            if (exit)
+                break;
+            switch (c) {
+                case '(' :
+                    s.push(c);
+                    break;
+                case ')' :
+                    if (!s.empty() && s.top() == '(')
+                        s.pop();
+                    else
+                        exit = true;
+                    break;
+                case '[' :
+                    s.push(c);
+                    break;
+                case ']' :
+                    if (!s.empty() && s.top() == '[')
+                        s.pop();
+                    else
+                        exit = true;
+                    break;
+            }
+        }
+        if (exit || !s.empty())
+            cout << "no\n";
+        else
+            cout << "yes\n";
+    }
+}
