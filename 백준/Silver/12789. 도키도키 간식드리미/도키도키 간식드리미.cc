675f69e82d9d59d7e5a8c40afdb4fe6299e07189
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-05 18:26:18 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    stack<int> s;
+    queue<int> q;
+    int n, input;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> input;
+        q.push(input);
+    }
+
+    int cur = 1;
+    while (!q.empty() || !s.empty()) {
+        if (q.front() == cur) {
+            q.pop();
+            cur++;
+        } else {  // q.front() > cur
+            if (s.empty()) {
+                s.push(q.front());
+                q.pop();
+            } else {  // s.empty == false
+                if (s.top() == cur) {
+                    s.pop();
+                    cur++;
+                } else if (s.top() > q.front()) {
+                    s.push(q.front());
+                    q.pop();
+                } else {  // s.top() < q.front()
+                    cout << "Sad" << '\n';
+                    return 0;
+                }
+            }
+        }
+    }
+    cout << "Nice" << '\n';
+}
